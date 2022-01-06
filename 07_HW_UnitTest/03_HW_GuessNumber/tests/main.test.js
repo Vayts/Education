361eaf97fe11c82adaa8d2cdb6191a60fdcac626
Aleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2022-01-07 00:11:05 +0200

    add 07_HW

---
+++ 07_HW_UnitTest/03_HW_GuessNumber/tests/main.test.js
@@ -0,0 +1,134 @@
+const {checkNumber, validateInput, guess, setSettings} = require('../src/homework/script/main')
+
+jest.mock('../src/homework/script/utils.js', () => {
+    const originalModule = jest.requireActual('../src/homework/script/utils.js');
+
+    //Mock the default export and named export 'foo'
+    return {
+        __esModule: true,
+        ...originalModule,
+        // alertCall: jest.fn(),
+        // attemptsPlus: jest.fn(),
+        getInputValue: jest.fn(() => true),
+        setInputValue: jest.fn(() => true),
+        addListener: jest.fn(() => true),
+        setTextValue: jest.fn(() => true),
+        toggleDisabledClass: jest.fn(() => true),
+    };
+});
+
+describe('guess', () => {
+    test('state with correct number', () => {
+        expect(guess({
+            gameNumber: 0,
+            userNumber: undefined,
+            gameAttempt: 12,
+            userAttempt: 0,
+            rangeMin: 0,
+            rangeMax: 200
+        })).toEqual(true)
+    })
+    test('state with uncorrect number', () => {
+        expect(guess({
+            gameNumber: 12,
+            userNumber: undefined,
+            gameAttempt: 12,
+            userAttempt: 0,
+            rangeMin: 0,
+            rangeMax: 200
+        })).toEqual(false)
+    })
+    test('state with userAttempt>gameAttempt', () => {
+        expect(guess({
+            gameNumber: 1,
+            userNumber: undefined,
+            gameAttempt: 12,
+            userAttempt: 13,
+            rangeMin: 0,
+            rangeMax: 200
+        })).toEqual(false)
+    })
+})
+
+describe('gameSettingsCheck', () => {
+    test('correct', () => {
+        expect(setSettings({
+            gameNumber: 12,
+            userNumber: undefined,
+            gameAttempt: 12,
+            userAttempt: 0,
+            rangeMin: 0,
+            rangeMax: 200
+        }, 10, 200, 10)).toEqual(true)
+    });
+    test('correct', () => {
+        expect(setSettings({
+            gameNumber: 12,
+            userNumber: undefined,
+            gameAttempt: 12,
+            userAttempt: 0,
+            rangeMin: 0,
+            rangeMax: 200
+        }, 10, 200, 16)).toEqual(false)
+    })
+})
+
+
+
+describe('checkNumber', () => {
+    test('15,100', () => {
+        expect(checkNumber(15, 100)).toEqual('Холодно')
+    })
+    test('0,100', () => {
+        expect(checkNumber(0, 100)).toEqual('Холодно')
+    })
+    test('105,100', () => {
+        expect(checkNumber(105, 100)).toEqual('Тепло')
+    })
+    test('"String",100', () => {
+        expect(checkNumber('string', 100)).toEqual('Я тебя не понимаю!')
+    })
+    test('105, "String"', () => {
+        expect(checkNumber(105, 'String')).toEqual('Я тебя не понимаю!')
+    })
+    test('null, "String"', () => {
+        expect(checkNumber(null, 'String')).toEqual('Я тебя не понимаю!')
+    })
+})
+
+describe('validateInput', () => {
+    test('1, 200, 7', () => {
+        expect(validateInput(1, 200, 7)).toEqual(true)
+    })
+    test('1, 3, 1', () => {
+        expect(validateInput(1, 3, 1)).toEqual(true)
+    })
+    test('199, 200, 7', () => {
+        expect(validateInput(198, 200, 1)).toEqual(true)
+    })
+    test('10, 15, 6', () => {
+        expect(validateInput(10, 15, 4)).toEqual(true)
+    })
+    test('1, 200, 21', () => {
+        expect(validateInput(1, 200, 21)).toEqual(false)
+    })
+    test('1, 220, 21', () => {
+        expect(validateInput(1, 220, 11)).toEqual(false)
+    })
+    test('199, 200, 7', () => {
+        expect(validateInput(199, 200, 7)).toEqual(false)
+    })
+    test('199, 155, 7', () => {
+        expect(validateInput(199, 155, 7)).toEqual(false)
+    })
+    test('"", 200, 7', () => {
+        expect(validateInput('', 200, 7)).toEqual(false)
+    })
+    test('199, "", 7', () => {
+        expect(validateInput(199, '', 7)).toEqual(false)
+    })
+    test('10, 15, 6', () => {
+        expect(validateInput(10, 15, 5)).toEqual(false)
+    })
+})
+
---
+++ 07_HW_UnitTest/04_HW_Randomizer/src/script/main.js
@@ -0,0 +1,94 @@
+const {removeDisabled, disabledCheck, setInputValue, getInputValue, setDisabled, addListener, setTextValue} = require("../../dist/script/utils");
+
+document.addEventListener('DOMContentLoaded', function(){
+    initApp();
+})
+
+function initApp() {
+    const state = {
+        rangeMin: 0,
+        rangeMax: 100,
+        arr: undefined,
+    }
+
+    addListener('generate-button', 'click', generate.bind(null, state))
+    addListener('reset-button', 'click', resetApp.bind(null, state))
+}
+
+function generate(state) {
+    if (!disabledCheck('min-input')) {
+        const min = Number(getInputValue('min-input'))
+        const max = Number(getInputValue('max-input'))
+        errorCheck(state, min, max)
+        removeDisabled('reset-button')
+    } else {
+        returnRandomNumber(state)
+    }
+}
+
+function errorCheck(state, min, max) {
+
+    if (min > 1000000 || max > 1000000) {
+        setTextValue('result-text','Numbers > 1 mil.')
+        return false;
+    } else if (min > max) {
+        setTextValue('result-text','Min > Max?')
+        return false
+    } else if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        setTextValue('result-text','Only integers')
+        return false
+    } else {
+        state.rangeMin = min
+        state.rangeMax = max
+        setDisabled('min-input');
+        setDisabled('max-input');
+        createArr(state);
+        returnRandomNumber(state)
+        return true;
+    }
+}
+
+function createArr(state) {
+    state.arr = []
+    for (let i = state.rangeMin; i <= state.rangeMax; i++) {
+        state.arr.push(i)
+    }
+    return state
+}
+
+function resetApp(state) {
+    state.arr = undefined;
+    state.rangeMin = 0;
+    state.rangeMax = 100;
+    setInputValue('min-input', state.rangeMin)
+    setInputValue('max-input', state.rangeMax)
+    setTextValue('result-text', '')
+    removeDisabled('min-input')
+    removeDisabled('max-input')
+    setDisabled('reset-button')
+
+    if(disabledCheck('generate-button')) {
+        removeDisabled('generate-button')
+    }
+    return state
+}
+
+function returnRandomNumber(state) {
+    if (state.arr.length === 0) {
+        setTextValue('result-text','Elements are over')
+        setDisabled('generate-button')
+        return false;
+    } else {
+        const randomIndex = random(0, state.arr.length-1)
+        const result = state.arr[randomIndex]
+        state.arr.splice(randomIndex, 1)
+        setTextValue('result-text', result)
+        return true;
+    }
+}
+
+function random(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = {errorCheck, returnRandomNumber, resetApp, createArr}
